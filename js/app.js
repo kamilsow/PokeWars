19c683ce10cd13cf099d89a2ca24b4566fc9f10d
kamilSowaLiane <kamil06076@gmail.com>
2020-06-14 15:05:21 +0200

    Simple web layout & random picker

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-console.log('Working!')
+const randomButtons = document.querySelectorAll('.pick')
+
+const randomPicker = () => {
+    var url = "https://pokeapi.co/api/v2/pokemon/" + (Math.floor(Math.random() * 807) + 1);
+    console.log(url)
+    fetch(url).then(data => data.json())
+        .then(jsonObject => {
+            console.log(jsonObject)
+        })
+}
+
+Array.from(randomButtons).forEach(button => {
+    button.addEventListener('click', randomPicker)
+})
