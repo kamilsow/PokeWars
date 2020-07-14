bb2ad2ec1db92da5e71d1fd354cb6ea0b05f77e3
kamilSowaLiane <kamil06076@gmail.com>
2020-07-14 23:10:13 +0200

    block chance for high def pokemons

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 // CARD CREATION
-const loader = `<div class="loader-wrapper"><div class="loader-text">Loading...</div><span class="loader"><span class="loader-inner"></span></span></div>`;
-const searchBoxes = document.querySelectorAll('.search-box');
-const messageBox = document.querySelector('.message-box');
+const loader = `<div class="loader-wrapper"><div class="loader-text">Loading...</div><span class="loader"><span class="loader-inner"></span></span></div>`
+const searchBoxes = document.querySelectorAll('.search-box')
+const messageBox = document.querySelector('.message-box')
 const picker = (i, query) => {
-    var url = "https://pokeapi.co/api/v2/pokemon/" + query;
+    var url = "https://pokeapi.co/api/v2/pokemon/" + query
     fetch(url).then(data => data.json())
         .then(jsonObject => {
             console.log(jsonObject)
@@ -16,7 +16,7 @@ const picker = (i, query) => {
 }
 const createPokeObject = (jsonObject) => {
     jsonObject.types.sort((a, b) => (a.slot > b.slot) ? 1 : -1)
-    typesOut = createType(jsonObject.types[0].type.name);
+    typesOut = createType(jsonObject.types[0].type.name)
     const pokemon = {
         imgFront: jsonObject.sprites.front_default,
         name: jsonObject.species.name,
@@ -56,26 +56,26 @@ const createPoke = (i, pokemon) => {
             <div>
         </div>
     `
-    document.querySelectorAll('.pokemon')[i].innerHTML = out;
+    document.querySelectorAll('.pokemon')[i].innerHTML = out
 }
 const createType = (pokeType) => {
     return `<p class="${pokeType}">${pokeType}</p>`
 }
 const checkType = () => {
-    let types = document.querySelectorAll('.type');
+    let types = document.querySelectorAll('.type')
     Array.from(types).forEach(type => {
-        let cardType = type.firstElementChild.textContent;
-        const typeArr = ['bug', 'dark', 'normal', 'fire', 'dragon', 'flying', 'electric', 'fairy', 'fighting', 'ghost', 'poison', 'grass', 'ground', 'ice', 'steel', 'psychic', 'rock', 'water'];
-        const strongAgainst = ['psychic', 'ghost', 'NONE', 'steel', 'bug', 'fighting', 'fire', 'dark', 'rock', 'fairy', 'grass', 'water', 'poison', 'dragon', 'ice', 'electric', 'flying', 'ground'];
-        const weakAgainst = ['dragon', 'fairy', 'NONE', 'electric', 'ice', 'rock', 'psychic', 'ghost', 'flying', 'dark', 'ground', 'poison', 'water', 'steel', 'fire', 'bug', 'fighting', 'grass'];
+        let cardType = type.firstElementChild.textContent
+        const typeArr = ['bug', 'dark', 'normal', 'fire', 'dragon', 'flying', 'electric', 'fairy', 'fighting', 'ghost', 'poison', 'grass', 'ground', 'ice', 'steel', 'psychic', 'rock', 'water']
+        const strongAgainst = ['psychic', 'ghost', 'NONE', 'steel', 'bug', 'fighting', 'fire', 'dark', 'rock', 'fairy', 'grass', 'water', 'poison', 'dragon', 'ice', 'electric', 'flying', 'ground']
+        const weakAgainst = ['dragon', 'fairy', 'NONE', 'electric', 'ice', 'rock', 'psychic', 'ghost', 'flying', 'dark', 'ground', 'poison', 'water', 'steel', 'fire', 'bug', 'fighting', 'grass']
         for (let i = 0; i < typeArr.length; i++) {
             if (cardType == typeArr[i]) {
                 type.parentElement.lastElementChild.children[1].children[1].innerHTML = `Strong against <p class="${strongAgainst[i]}">${strongAgainst[i]}</p>`
                 type.parentElement.lastElementChild.children[1].children[2].innerHTML = `Weak against <p class="${weakAgainst[i]}">${weakAgainst[i]}</p>`
-                type.parentElement.firstElementChild.classList.add(typeArr[i]);
-                type.parentElement.children[3].classList.add(typeArr[i] + '2');
-                type.parentElement.classList.add(typeArr[i] + '2');
-                break;
+                type.parentElement.firstElementChild.classList.add(typeArr[i])
+                type.parentElement.children[3].classList.add(typeArr[i] + '2')
+                type.parentElement.classList.add(typeArr[i] + '2')
+                break
             }
         }
     })
@@ -91,7 +91,7 @@ const createHpBar = (pokemon, i) => {
             <div class="health-bar-text">100%</div>
         </div>
     `
-    document.querySelectorAll('.pokemon')[i].innerHTML += out;
+    document.querySelectorAll('.pokemon')[i].innerHTML += out
 }
 for (let i = 0; i < 2; i++) {
     document.querySelectorAll('.pick-random')[i].addEventListener('click', function () {
@@ -99,7 +99,7 @@ for (let i = 0; i < 2; i++) {
         const query = (Math.floor(Math.random() * 807) + 1)
         picker(i, query)
     })
-    document.querySelectorAll('.pick-search')[i].addEventListener('click', function() {
+    document.querySelectorAll('.pick-search')[i].addEventListener('click', function () {
         document.querySelectorAll('.pokemon')[i].innerHTML = loader
         const query = searchBoxes[i].value
         picker(i, query)
@@ -109,10 +109,10 @@ for (let i = 0; i < 2; i++) {
 /* 
     %DEF reduction (max 90%)
 */
-document.querySelector("#stop").addEventListener('click', function() { console.log(Math.floor(Math.random() * 100 + 1))})
+document.querySelector("#stop").addEventListener('click', function () { console.log(Math.floor(Math.random() * 100 + 1)) })
 document.querySelector("#start").addEventListener('click', function () {
     messageBox.innerHTML += `Battle starts in 3 sec<br />`
-    let counter = 0;
+    let counter = 0
     const pokeOne = document.querySelectorAll('.card')[0].firstElementChild.textContent.charAt(0).toLocaleUpperCase() + document.querySelectorAll('.card')[0].firstElementChild.textContent.slice(1)
     const pokeTwo = document.querySelectorAll('.card')[1].firstElementChild.textContent.charAt(0).toLocaleUpperCase() + document.querySelectorAll('.card')[1].firstElementChild.textContent.slice(1)
     const pokeOneMaxHealth = parseInt(document.querySelectorAll('.total')[0].innerHTML.substring(0, Math.floor(document.querySelectorAll('.total')[0].innerHTML.length / 2)))
@@ -141,151 +141,177 @@ document.querySelector("#start").addEventListener('click', function () {
         messageBox.innerHTML += `Poke two deals additional 50% damage because he is strong against poke one <br />`
     }
     const pokeOneAttack = () => {
-        const i = 1;
-        let dodgeChance = 5
+        const i = 1
+        let dodgeChance = 5 //it is dodge chance of pokemon two
         let critChance = 5
         let damage = pokeOneAtt
         let dodgeRNG = Math.floor(Math.random() * 100 + 1)
         let critRNG = Math.floor(Math.random() * 100 + 1)
         let reduction = pokeTwoDef * 0.01
         let additionalInfo = ``
-        if (pokeTwoSpeed > 50) {
+        // For every 5 points of speed above 50, pokemon get +1% to dodge chance
+
+        if (pokeTwoDef > 90) {
+            let blockChance = Math.floor((pokeTwoDef - 90) / 3)
+            let blockRNG = Math.floor(Math.random() * 100 + 1)
+            dodgeChance = 0
+            if (blockRNG <= blockChance) {
+                damage = 0
+                messageBox.innerHTML += `${pokeTwo} blocks incomming damage. <br />`
+                updateScroll()
+                return
+            }
+        } else if (pokeTwoSpeed > 50) {
+            console.log('odpalilo sie sprawdzanie dodga pokemona 2')
             dodgeChance += Math.floor((pokeTwoSpeed - 50) / 5)
             console.log(dodgeChance)
+            if (dodgeRNG <= dodgeChance) {
+                // Checking if pokemon dodges the attack (Base dodge chance is 5%, dodgeRNG is random number between 1 and 100 and determines dodge success)
+                damage = 0
+                messageBox.innerHTML += `${pokeTwo} dodges and takes no damage. <br />`
+                updateScroll()
+                return
+            }
         }
-        // Checking if pokemon dodges the attack (Base dodge chance is 5%, dodgeRNG is random number between 1 and 100 and determines dodge success)
-        if (dodgeRNG <= dodgeChance) {
-            damage = 0
-            messageBox.innerHTML += `${pokeTwo} dodges and takes no damage. <br />`
+        // normal battle
+        // max 90% dmg reducted (more than 90 gives block chance)
+        if (pokeTwoDef >= 90) {
+            reduction = 0.9
+        }
+        if (critRNG <= critChance) {
+            damage += damage * 0.5
+            additionalInfo += `(+50% crit)`
+        }
+        // Base dmg
+        damage = (1 - reduction) * damage
+        // Dmg range 
+        let min = (damage * 0.8).toFixed(1)
+        let max = (damage * 1.2).toFixed(1)
+        console.log(min + ' ' + max)
+        let calc = (Math.random() * (Number(max) - Number(min)) + Number(min)).toFixed(1)
+        damage = calc
+        pokeTwoCurHealth = (pokeTwoCurHealth - damage).toFixed(1)
+        if (pokeTwoCurHealth <= 0) {
+            messageBox.innerHTML += `${pokeTwo} took ${damage} points of damage and died. ${pokeOne} wins! ${additionalInfo}`
+            pokeTwoCurHealth = 0
             updateScroll()
+            end()
         } else {
-            // if not -> normal battle
-            // max 90% dmg reducted (more than 90 defense doesn't count)
-            if (pokeTwoDef >= 90) {
-                reduction = 0.9
-            }
-            if (critRNG <= critChance) {
-                damage += damage * 0.5
-                additionalInfo += `(+50% crit)`
-            }
-            // Base dmg
-            damage = (1 - reduction) * damage
-            // Dmg range 
-            let min = (damage * 0.8).toFixed(1)
-            let max = (damage * 1.2).toFixed(1)
-            console.log(min + ' ' + max)
-            let calc = (Math.random() * (Number(max) - Number(min)) + Number(min)).toFixed(1)
-            damage = calc
-            pokeTwoCurHealth = (pokeTwoCurHealth - damage).toFixed(1);
-            if (pokeTwoCurHealth <= 0) {
-                messageBox.innerHTML += `${pokeTwo} took ${damage} points of damage and died. ${pokeOne} wins!`
-                pokeTwoCurHealth = 0;
-                updateScroll()
-                end();
-            } else {
-                messageBox.innerHTML += `${pokeTwo} took ${damage} points of damage ${additionalInfo}<br />`
-                updateScroll()
-            }
+            messageBox.innerHTML += `${pokeTwo} took ${damage} points of damage ${additionalInfo}<br />`
+            updateScroll()
         }
-        applyChange(pokeTwoCurHealth, pokeTwoMaxHealth, i);
+        applyChange(pokeTwoCurHealth, pokeTwoMaxHealth, i)
     }
     const pokeTwoAttack = () => {
-        const i = 0;
+        const i = 0
         let dodgeChance = 5
         let critChance = 5
-        let damage = pokeTwoAtt 
+        let damage = pokeTwoAtt
         let dodgeRNG = Math.floor(Math.random() * 100 + 1)
         let critRNG = Math.floor(Math.random() * 100 + 1)
-        let reduction = pokeOneDef * 0.01 
+        let reduction = pokeOneDef * 0.01
         let additionalInfo = ``
-        if (pokeOneSpeed > 50) {
+
+        if (pokeOneDef > 90) {
+            let blockChance = Math.floor((pokeOneDef - 90) / 3)
+            let blockRNG = Math.floor(Math.random() * 100 + 1)
+            dodgeChance = 0
+            if (blockRNG <= blockChance) {
+                damage = 0
+                messageBox.innerHTML += `${pokeOne} blocks incomming damage. <br />`
+                updateScroll()
+                return
+            }
+        } else if (pokeOneSpeed > 50) {
+            console.log('odpalilo sie sprawdzanie dodga pokemona 1')
             dodgeChance += Math.floor((pokeOneSpeed - 50) / 5)
             console.log(dodgeChance)
+            if (dodgeRNG <= dodgeChance) {
+                damage = 0
+                messageBox.innerHTML += `${pokeOne} dodges and takes no damage. <br />`
+                updateScroll()
+                return
+            }
         }
-        if (dodgeRNG <= dodgeChance) {
-            damage = 0
-            messageBox.innerHTML += `${pokeOne} dodges and takes no damage <br />.`
+
+        if (pokeOneDef >= 90) {
+            reduction = 0.9
+        }
+        if (critRNG <= critChance) {
+            damage += damage * 0.5
+            additionalInfo += `(+50% crit)`
+        }
+        damage = (1 - reduction) * damage
+        let min = (damage * 0.8).toFixed(1)
+        let max = (damage * 1.2).toFixed(1)
+        console.log(min + ' ' + max)
+        let calc = (Math.random() * (Number(max) - Number(min)) + Number(min)).toFixed(1)
+        damage = calc
+        pokeOneCurHealth = (pokeOneCurHealth - damage).toFixed(1)
+        if (pokeOneCurHealth <= 0) {
+            messageBox.innerHTML += `${pokeOne} took ${damage} points of damage and died. ${pokeTwo} wins! ${additionalInfo}`
+            pokeOneCurHealth = 0
             updateScroll()
+            end()
         } else {
-            if (pokeOneDef >= 90) {
-                reduction = 0.9
-            }
-            if (critRNG <= critChance) {
-                damage += damage * 0.5
-                additionalInfo += `(+50% crit)`
-            }
-            damage = (1 - reduction) * damage
-            let min = (damage * 0.8).toFixed(1)
-            let max = (damage * 1.2).toFixed(1)
-            console.log(min + ' ' + max)
-            let calc = (Math.random() * (Number(max) - Number(min)) + Number(min)).toFixed(1)
-            damage = calc
-            pokeOneCurHealth = (pokeOneCurHealth - damage).toFixed(1);
-            if (pokeOneCurHealth <= 0) {
-                messageBox.innerHTML += `${pokeOne} took ${damage} points of damage and died. ${pokeTwo} wins!`
-                pokeOneCurHealth = 0;
-                updateScroll()
-                end();
-            } else {
-                messageBox.innerHTML += `${pokeOne} took ${damage} points of damage ${additionalInfo}<br />`
-                updateScroll()
-            }
+            messageBox.innerHTML += `${pokeOne} took ${damage} points of damage ${additionalInfo}<br />`
+            updateScroll()
         }
-        applyChange(pokeOneCurHealth, pokeOneMaxHealth, i);
+        applyChange(pokeOneCurHealth, pokeOneMaxHealth, i)
     }
-    async function turn () {
-        counter ++
+    async function turn() {
+        counter++
         messageBox.innerHTML += `Turn ${counter}<br />`
         if (pokeOneSpeed > pokeTwoSpeed) {
             if (pokeOneCurHealth > 0) {
                 await new Promise((resolve, reject) => {
                     setTimeout(() => {
-                        resolve(pokeOneAttack());
-                    }, 1500);
-                });
+                        resolve(pokeOneAttack())
+                    }, 1500)
+                })
             }
         }
         if (pokeTwoCurHealth > 0) {
             await new Promise((resolve, reject) => {
                 setTimeout(() => {
-                    resolve(pokeTwoAttack());
-                }, 1500);
+                    resolve(pokeTwoAttack())
+                }, 1500)
             })
         }
         if (pokeTwoSpeed > pokeOneSpeed) {
             if (pokeOneCurHealth > 0) {
                 await new Promise((resolve, reject) => {
                     setTimeout(() => {
-                        resolve(pokeOneAttack());
-                    }, 1500);
-                });
+                        resolve(pokeOneAttack())
+                    }, 1500)
+                })
             }
         }
     }
     interval = setInterval(turn, 3100)
-    function end () {
+    function end() {
         clearInterval(interval)
     }
-});
+})
 
 function applyChange(curHealth, maxHealth, i) {
-    var a = curHealth * (100 / maxHealth);
-    document.querySelectorAll('.health-bar-text')[i].innerHTML = (Math.round(a) + "%");
+    var a = curHealth * (100 / maxHealth)
+    document.querySelectorAll('.health-bar-text')[i].innerHTML = (Math.round(a) + "%")
 
-    document.querySelectorAll(".health-bar-red")[i].classList.add('animate');
-    setTimeout(() => document.querySelectorAll(".health-bar-red")[i].classList.remove('animate'), 1600);
-    document.querySelectorAll(".health-bar-red")[i].style.width = (a + "%");
+    document.querySelectorAll(".health-bar-red")[i].classList.add('animate')
+    setTimeout(() => document.querySelectorAll(".health-bar-red")[i].classList.remove('animate'), 1600)
+    document.querySelectorAll(".health-bar-red")[i].style.width = (a + "%")
 
-    document.querySelectorAll(".health-bar")[i].classList.add('animate');
-    setTimeout(() => document.querySelectorAll(".health-bar")[i].classList.remove('animate'), 200);
+    document.querySelectorAll(".health-bar")[i].classList.add('animate')
+    setTimeout(() => document.querySelectorAll(".health-bar")[i].classList.remove('animate'), 200)
     document.querySelectorAll(".health-bar")[i].style.width = (a + "%")
 
-    document.querySelectorAll(".health-bar-blue")[i].classList.add('.animate');
-    setTimeout(() => document.querySelectorAll(".health-bar-blue")[i].classList.remove('animate'), 700);
+    document.querySelectorAll(".health-bar-blue")[i].classList.add('.animate')
+    setTimeout(() => document.querySelectorAll(".health-bar-blue")[i].classList.remove('animate'), 700)
     document.querySelectorAll(".health-bar-blue")[i].style.width = (a + "%")
 
-    document.querySelectorAll('.total')[i].innerHTML = (curHealth + "/" + maxHealth);
+    document.querySelectorAll('.total')[i].innerHTML = (curHealth + "/" + maxHealth)
 }
-function updateScroll(){
-    messageBox.scrollTop = messageBox.scrollHeight;
+function updateScroll() {
+    messageBox.scrollTop = messageBox.scrollHeight
 }
